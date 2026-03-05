48215d2d36c72cb1b72cba0fc79ee597ffa1384e
Jimmy <jimmy@r3e.network>
2026-03-05 12:15:17 +0800

    chore(frontend): cleanup text scripts

--- frontend/test_docsview7.js
+++
@@ -1,9 +0,0 @@
-import { Marked } from 'marked';
-const marked = new Marked();
-const renderer = new marked.Renderer();
-renderer.code = (token) => {
-  return "MAPPED:" + token.lang;
-};
-marked.use({ renderer });
-
-console.log(marked.parse("```mermaid\ngraph TD\n```"));
